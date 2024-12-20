90b9ea96ac7cab8399dfc4b6a9bcb5ed04ad6c80
Agni Nugroho <agniwwe@gmail.com>
2024-12-20 21:48:08 +0700

    admin page halaman laporan

--- admin/terima.php
+++ admin/terima.php
@@ -5,6 +5,6 @@ if (isset($_GET['id_pengaduan'])) {
     $query = mysqli_query($conn, "UPDATE pengaduan SET status = 'selesai' WHERE pengaduan.id_pengaduan = '" . $_GET['id_pengaduan'] . "'");
     header("location: laporan.php");
 } else {
-    exit("Halaman tidak dapat diakses!")
+    exit("Halaman tidak dapat diakses!");
 }
 ?>
